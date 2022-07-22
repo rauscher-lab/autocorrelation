2eafb9b2ec6ff95f6b9cb9cae0e5744f4d1e6ffd
Eugene Klyshko <klyshko.jack@gmail.com>
2022-07-22 11:29:48 -0400

    Update autocorrelation.py

--- autocorrelation.py
+++ autocorrelation.py
@@ -4,27 +4,26 @@ import numpy as np
 from scipy.signal import correlate
 from scipy.optimize import curve_fit
 
-
 def one_hot_encode(num, max_num):
-    vector = np.zeros(max_num, dtype=np.int32)
+    vector = np.zeros(max_num, dtype=np.bool)
     if num > 0:
         vector[num-1] = True 
     return vector
     
-def vector_autocorrelate(ts_array):
+def vector_autocorrelate(ts_array, dtype=float):
     n_timepoints = len(ts_array)
     n_dimensions = len(ts_array[0])
-    acorr = [np.correlate(ts_array[:,i],ts_array[:,i],'full') for i in range(n_dimensions)]# correlate each component independently
+    acorr = [np.correlate(ts_array[:,i].astype(dtype), ts_array[:,i].astype(dtype),'full') for i in range(n_dimensions)]# correlate each component independently
     acorr = np.array(acorr)[:,n_timepoints-1:] # take the second half
     acorr = np.sum(acorr, axis = 0) # sum the correlations for each component
     acorr = acorr * 1./(n_timepoints - np.arange(n_timepoints)) # divide by the number of values actually measured and return
     return acorr
 
-def vector_autocorrelate_signal(ts_array):
+def vector_autocorrelate_signal(ts_array, dtype=float):
     n_time = ts_array.shape[0]
     n_dim = ts_array.shape[1]
-    acorr = np.array([correlate(ts_array[:,i],ts_array[:,i],'full') for i in range(n_dim)])# correlate each component independently
-    acorr = acorr[:, n_time-1:]
+    acorr = np.array([correlate(ts_array[:,i].astype(dtype), ts_array[:,i].astype(dtype),'full') for i in range(n_dim)])# correlate each component independently
+    acorr = acorr[:, n_time-1:].astype(float)
     acorr = np.sum(acorr, axis = 0) # sum the correlations for each component
     acorr = acorr * 1./(n_time - np.arange(n_time)) # divide by the number of values actually measured and return
     return acorr
@@ -35,7 +34,7 @@ def index_autocorrelate_old(index_ts):
     ts_corrected = np.array([it2id[item] for item in index_ts])
     oh_ts = [one_hot_encode(corrected_index, max(ts_corrected)) for corrected_index in ts_corrected]
     oh_ts = np.array(oh_ts)
-    ac = vector_autocorrelate(oh_ts)
+    ac = vector_autocorrelate(oh_ts, dtype=np.int8)
     return ac
 
 def index_autocorrelate(mol_ts):
@@ -44,23 +43,24 @@ def index_autocorrelate(mol_ts):
     unique_indexes = np.unique(indexes)
         
     if np.all(unique_mols == 0) and len(unique_mols) == 1: # if no molecules found
-        oh_ts = np.zeros((len(mol_ts), 1), dtype=np.int32)
+        oh_ts = np.zeros((len(mol_ts), 1), dtype=np.bool)
         
     elif np.all(unique_indexes == 0) and len(unique_mols) == 1: # if the same one molecule found
-        oh_ts = np.ones((len(mol_ts), 1), dtype=np.int32)
+        oh_ts = np.ones((len(mol_ts), 1), dtype=np.bool)
         
     elif np.all(unique_mols > 0) and  len(unique_mols) > 1: # if different with no zero
-        oh_array = np.zeros((unique_indexes.shape[0] + 1, max((np.max(indexes + 1), 1))), dtype=np.int32)
+        oh_array = np.zeros((unique_indexes.shape[0] + 1, max((np.max(indexes + 1), 1))), dtype=np.bool)
         for un_index in unique_indexes + 1:
             oh_array[un_index] = one_hot_encode(un_index, max((np.max(indexes + 1), 1)))
         oh_ts = np.take(oh_array, indexes+1, axis=0)
         
     elif np.any(unique_mols == 0) and len(unique_mols) > 1: # if different ones (including 0)
-        oh_array = np.zeros((unique_indexes.shape[0], max((np.max(indexes), 1))), dtype=np.int32)
+        oh_array = np.zeros((unique_indexes.shape[0], max((np.max(indexes), 1))), dtype=np.bool)
         for un_index in unique_indexes:
             oh_array[un_index] = one_hot_encode(un_index, max((np.max(indexes), 1)))
         oh_ts = np.take(oh_array, indexes, axis=0) 
-    ac = vector_autocorrelate_signal(oh_ts)
+    ac = vector_autocorrelate_signal(oh_ts, dtype=np.int8)
+    print(ac)
     return ac
 
 def auto_window(taus, c):
@@ -93,4 +93,3 @@ def autocorrelation_time_fit(ac_norm):
     popt, pcov = curve_fit(exp_f, np.arange(len(ac_norm)), ac_norm)
     tau_est = popt[0]
     return tau_est
-    
